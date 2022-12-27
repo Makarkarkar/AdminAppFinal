4d0a4f35002e03dafcf199d4d3e42b0d6fdf30c5
Makarkarkar <82977966+Makarkarkar@users.noreply.github.com>
2022-12-27 14:58:14 +0300

    Update HomeController.cs

--- serviceone/Controllers/HomeController.cs
+++ serviceone/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ public class HomeController : ControllerBase
     public HomeController()
     {
         _client = new HttpClient();
-        _client.BaseAddress = new Uri(Environment.GetEnvironmentVariable("WeatherService")!);
+        _client.BaseAddress = new Uri(Environment.GetEnvironmentVariable("ServiceTwoAddres")!);
         _client.DefaultRequestHeaders.Accept.Add(
             new MediaTypeWithQualityHeaderValue("application/json"));
     }
